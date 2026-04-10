10cf6b9695ca0ad789c0dcac117d5499e4478e5b
IvanKhudyakov <halex7@yandex.ru>
2026-04-10 19:42:01 +0300

    Add switch between main and login page V2 with crossing import

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,9 @@
 from .pages.main_page import MainPage
-from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.should_be_login_link()
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
+    login_page = main_page.go_to_login_page()
     login_page.should_be_login_page()
